e9bb5cf3a5bd55d7dd9f87dcedfa05456950fe7f
Eugene-Kwaka <kwakaeugene@gmail.com>
2022-05-17 12:42:49 +0300

    before deployment

--- BlogSite/settings.py
+++ BlogSite/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -95,10 +96,18 @@ WSGI_APPLICATION = 'BlogSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
+    
 }
+
 
 
 # Password validation
@@ -214,3 +223,6 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#HEROKU SETTINGS
+django_on_heroku.settings(locals())
